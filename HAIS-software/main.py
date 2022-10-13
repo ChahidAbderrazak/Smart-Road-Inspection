e94a274db37e106e1b0c4e6037eb6975f839a39b
Abderrazak Chahid <abderrazak.chahid@gmail.com>
2022-10-13 13:29:38 -0400

    generate Nuscene-like database

--- HAIS-software/main.py
+++ HAIS-software/main.py
@@ -1,34 +1,35 @@
-from telecom_module.lib import utils_telecom
-from inspection_module.lib import utils
-from inspection_module.lib import hais_database
 
-def sync_data_to_firebase():
-    #  retreive the the inspection data from the cloud
-    utils_telecom.download_data()
+from database_module.lib import hais_database
 
-    # send the inspection data to the cloud
-    utils_telecom.upload_data()
+# from telecom_module.lib import utils_telecom
+# def sync_data_to_firebase():
+#     #  retreive the the inspection data from the cloud
+#     utils_telecom.download_data()
+
+#     # send the inspection data to the cloud
+#     utils_telecom.upload_data()
 
 def convert_measurment_to_database():
 	# collected data input
-	raw_data = hais_database.HAIS_bot(config='config/config.json')
+	raw_data = hais_database.HAIS_node(config='config/config.json')
 
-	# # create the database structure
-	# raw_data.create_database()
+	# create the database structure
+	raw_data.create_database()
 
-	# annotate the a scene
+	# Show a scene 
 	scene_=raw_data.scene
 	print(f'\n\n - scene= {scene_}')
-	raw_data.annotate_database(scene=scene_[0])
 
+	# # annotate the a scene
+	# raw_data.annotate_database(scene=scene_[0])
 
 if __name__ == "__main__":
 
-    # convert the sensors measurment to a structured HAIS-database
+    # convert the sensors measurment to a structured HAIS-database: "/media/abdo2020/DATA1/Datasets/data-demo/hais-inspection/",
     convert_measurment_to_database()
 
-    # synchorize (upload/download) sensors measurement
-    sync_data_to_firebase()
+    # # synchorize (upload/download) sensors measurement
+    # sync_data_to_firebase()
 
 
 
